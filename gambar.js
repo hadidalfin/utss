b3cd746af50e6766b40eef16e4198ca452749f01
hadidalfin <hadidalfin@gmail.com>
2022-04-20 18:32:25 +0700

    utss

---
+++ gambar.js
@@ -0,0 +1,13 @@
+var swiper = new Swiper(".swiper", {
+        spaceBetween: 30,
+        centeredSlides: true,
+        autoplay: {
+          delay: 2500,
+          disableOnInteraction: false,
+        },
+        navigation: {
+          nextEl: ".swiper-button-next",
+          prevEl: ".swiper-button-prev",
+        },
+
+      });
